63459599295e98ce63f12a15a0fc5a9ea42bfcdc
paulotfchaves <43656788+paulotfchaves@users.noreply.github.com>
2024-11-13 07:55:09 -0300

    clear

--- src/routes/facebookApiRequest.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import FacebookApiRequestController from "../controllers/facebookApiRequest";
-
-const router = Router();
-
-router.post("/send-event", FacebookApiRequestController.sendEvent);
-
-export default router;
